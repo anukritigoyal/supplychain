8ce0ebeabb8c5172d873a342ee916cb3fe4a0c11
GuyFawkes <isaac.prasanth@gmail.com>
2018-06-14 19:25:30 +0530

    State public key visibilty

--- webapp/items/sawtooth/send.py
+++ webapp/items/sawtooth/send.py
@@ -13,7 +13,7 @@ def snd(name,nxt_add,usrname):
 	keyfile = _get_keyfile(usrname)
 	client = HwClient(base_url=url,keyfile = keyfile)
 
-	response = client.send(name=name,nxt_add=nxt_add,cu_add = username)
+	response = client.send(name=name,nxt_add=nxt_add,cu_add = usrname)
 
 	print("response: {}".format(response))
 
