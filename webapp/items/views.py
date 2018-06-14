4e6b66e9ea0eb3548ae54e3d58ab884f4355dcd3
GuyFawkes <isaac.prasanth@gmail.com>
2018-06-14 21:32:48 +0530

    index view

--- webapp/items/views.py
+++ webapp/items/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render,get_object_
 from .sawtooth import querying
-from django.htpp import Http404
+from django.http import Http404
 
 # Create your views here.
 
 def index(request):
 	response = querying.query_all_items()
 	return render(request,'items/index.html',response)
-
+'''
 def detail(request,itemname):
 	try:
 		response = finder.find(itemname,'ubuntu')
@@ -15,3 +15,4 @@ def detail(request,itemname):
 		raise Http404("Item Doesn't exist")
 		
 	return render(request,'items/detail.html',response)	
+'''
