2f20f4d3d370db12b5206452d26a7915523a4a19
GuyFawkes <isaac.prasanth@gmail.com>
2018-06-14 22:49:23 +0530

    debug

--- webapp/items/views.py
+++ webapp/items/views.py
@@ -8,17 +8,41 @@ from .sawtooth import finder
 def index(request):
 	response = querying.query_all_items()
 	print(response)
+	#json to dict
 	#response = {1: {name:jo}, 2:{name:sj}}
-	return render(request,'items/index.html',{reponse : response})
+
+	return render(request,'items/index.html', dictresp)
 
 def detail(request,itemname):
 
 	try:
 		response = finder.find(itemname,'ubuntu')
+		dictresp = _deserialize(response)
+
 	except:
 		raise Http404("Item Doesn't exist")
 
-	return render(request,'items/detail.html',response)	
+	return render(request,'items/detail.html',dictresp)	
 
 def create(request):
 	return None
+
+
+class Item(object):
+	def __init__(self,name,check,c_addr,p_addr):
+		self.name = name
+		self.check = check
+		self.c_addr = c_addr
+		self.p_addr = p_addr
+
+def _deserialize(data):
+		items = {}
+		try:
+			for item in data.decode().split("|"):
+				name,check,c_addr,p_addr = item.split(",")
+				items[name] = Item(name,check,c_addr,p_addr)
+
+		except ValueError:
+			raise InternalError("Failed to deserialize items data")
+
+		return items
