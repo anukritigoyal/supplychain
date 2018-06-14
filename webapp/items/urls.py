e8e95dbb1fd1e13ba886399f129d67be809aa0ae
GuyFawkes <isaac.prasanth@gmail.com>
2018-06-14 22:02:14 +0530

    debug

--- webapp/items/urls.py
+++ webapp/items/urls.py
@@ -7,7 +7,8 @@ import time
 urlpatterns = [
 
 	url('',views.index,name='index'),
-#	url('<str:itemname>/',views.detail,name='detail'),
+	url('create',views.create,name='create')
+	url('<str:itemname>/',views.detail,name='detail'),
 
 
 ]
