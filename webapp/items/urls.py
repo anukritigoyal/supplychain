f731c3a0db286fd73f660203fee0780be5be28e2
GuyFawkes <isaac.prasanth@gmail.com>
2018-06-15 16:51:37 +0530

    login

--- webapp/items/urls.py
+++ webapp/items/urls.py
@@ -8,7 +8,7 @@ import time
 urlpatterns = [
 	
 	url('home/',views.index,name='index'),
-	url('login/',views.UserFormView.as_view(),name= 'login')
+	url('login/',views.UserFormView.as_view(),name= 'login'),
 	url('create/',views.create,name='create'),
 	path('details/<itemname>/',views.detail,name='detail'),
 	path('details/<itemname>/checks/',views.checked , name = 'checked')
