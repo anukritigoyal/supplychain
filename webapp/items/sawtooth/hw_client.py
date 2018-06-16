2eeab5b71085aeea54395e05c14d65586fd8fa2c
GuyFawkes <isaac.prasanth@gmail.com>
2018-06-17 05:10:43 +0530

    intc

--- webapp/items/sawtooth/hw_client.py
+++ webapp/items/sawtooth/hw_client.py
@@ -170,7 +170,7 @@ class HwClient:
 		signature = self._signer.sign(header)
 		transaction = Transaction(header= header,payload = payload,
 			header_signature = signature)
-
+		print("t.p")
 		print(transaction.payload)
 
 		batch_list = self._create_batch_list([transaction])
