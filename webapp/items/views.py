dd8ccc27c8e635b8b5ed2c43b3fe07675c93e53e
GuyFawkes <isaac.prasanth@gmail.com>
2018-06-16 19:20:14 +0530

    debug create

--- webapp/items/views.py
+++ webapp/items/views.py
@@ -148,7 +148,7 @@ class UserFormView(View):
 
 	def post(self,request):
 		form = self.form_class(request.POST)
-		username = request.POST['username']
+		username = request.user.username
 		password  =request.POST['password']
 		user = authenticate(username=username,password=password)
 
