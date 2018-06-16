821d284e9161ec660fb2069a3fc4b72563cb5e79
GuyFawkes1 <39523317+GuyFawkes1@users.noreply.github.com>
2018-06-17 03:23:43 +0530

    Webapp (#6)
    * login
    
    * login
    
    * login
    
    * map
    
    * map
    
    * map
    
    * map
    
    * map
    
    * map
    
    * map
    
    * map
    
    * map
    
    * map
    
    * map
    
    * map
    
    * map
    
    * map
    
    * map
    
    * map
    
    * map
    
    * map
    
    * map
    
    * map
    
    * map
    
    * map
    
    * map
    
    * map
    
    * map
    
    * map
    
    * Create and Logout
    
    * readme
    
    * creation and logout
    
    * add = in if
    
    * .hw
    
    * vscode
    
    * form forms
    
    * creation model
    
    * creation
    
    * create view debug
    
    * debug
    
    * authentication
    
    * authentication
    
    * entire authentication
    
    * authentication
    
    * debig
    
    * debug login
    
    * debug create
    
    * debig
    
    * w
    
    * deletion of spaces
    
    * new host
    
    * debug login again
    
    * logout
    
    * itemcreate
    
    * debug
    
    * debug
    
    * debug create
    
    * create debug
    
    * change cu_add
    
    * c
    
    * wal changes
    
    * wal

--- webapp/items/forms.py
+++ webapp/items/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django import forms
+from .models import ItemCreation
 
 class UserForm(forms.ModelForm):
 	password = forms.CharField(widget=forms.PasswordInput)
@@ -7,3 +8,15 @@ class UserForm(forms.ModelForm):
 	class Meta:
 		model = User
 		fields = ['username','email','password']
+
+
+#Create a form for creation of item
+#Code reuse :D
+
+#probably will hit me 
+class CreateItemForm(forms.ModelForm):
+	password = forms.CharField(widget=forms.PasswordInput)
+
+	class Meta:
+		model = ItemCreation
+		fields = ['itemname','password']
--- webapp/items/views.py
+++ webapp/items/views.py
@@ -1,40 +1,49 @@
 from django.shortcuts import render,redirect
-from .sawtooth import querying
 from django.http import Http404
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login , logout
+from .sawtooth import querying
+from .sawtooth import send
+from .sawtooth import create as create_saw
 from .sawtooth import finder as finder_saw
 from .sawtooth import his
 from .sawtooth import checks
-import json
 import time
 from profiles.wallet import finder as finder_wal
 from .forms import UserForm
+from .forms import CreateItemForm
 from django.views import View
-# Create your views here.
+
+#imported and not used create,send
 
 
 def index(request):
-	#response = querying.query_all_items()
-	response = querying.query_user_held(request.user.username)
 	
+	if request.user.is_authenticated == False :
+		return redirect('items:login')
+
+
+	response = querying.query_user_held(request.user.username)
+	#returns from state table all the datas with c_add as username
+
 	resp = {}
 	for s in response:
 		name,checks,c_add,prev_add = response[s].decode().split(",")
+		
+		#finding out human name of the public key holder
 		nc_add = finder_wal.query(c_add,request.user.username)
 		nc_add = _deserialize_key(nc_add)
-
-		'''np_add = finder.query(prev_add,'ubuntu')
-								np_add = _deserialize_key(np_add)
-								'''
 		resp[name] = Item(name,checks,nc_add,prev_add)
 
-
-	
 	context = {'resp' :resp}
 
 	return render(request,'items/index.html', context)
 
 def detail(request,itemname):
+	
+	if request.user.is_authenticated == False == False :
+		return redirect('items:login')
+
+
 	#find item uses state list 
 	response = finder_saw.find(itemname,'ubuntu')
 	
@@ -44,18 +53,25 @@ def detail(request,itemname):
 	resp[itemname].c_addr = nc_add
 	#get the checks list
 	checks_list = checks.item_checks_list(resp[itemname].check)
-	#hist goes through transactions in BC, so returns in human readble form
+	#hist goes through transactions in block chain, so returns in human readble form
+	
+	#serialized make that into an item history class with all the attributes so that django 
+	#will not complain
+	#we can do the serializtion and breaking up stuff in the his.py
 	hist= his.item_history(itemname)
-	print(resp[itemname].check)
-	print(resp[itemname].name)
-
-
 
 	context = {'resp' :resp,'hist' : hist , "checks_list" : checks_list}
 	return render(request,'items/detail.html',context)	
 
 def checked(request,itemname):
+
+	if request.user.is_authenticated == False == False :
+		return redirect('items:login')
+
+
 	checks.check(itemname,'ubuntu',request.POST['check'],'ubuntu')
+	#necessary because it takes atleast two secs for the state list to get updated
+	#should find a more robust way to do this
 	time.sleep(2)
 	response = finder_saw.find(itemname,'ubuntu')
 	
@@ -72,39 +88,82 @@ def checked(request,itemname):
 	return render(request,'items/detail.html',context)
 
 
+def map(request):
+	
+	if request.user.is_authenticated == False :
+		return redirect('items:login')
+
+	response = querying.query_all_items()
+	resp = {}
+	for s in response:
+		name,checks,c_add,prev_add = response[s].decode().split(",")
+		nc_add = finder_wal.query(c_add,'ubuntu')
+		nc_add = _deserialize_key(nc_add)
+		resp[name] = Item(name,checks,nc_add,prev_add)
+		
+	context = {'resp' :resp}
+	return render(request,'items/map.html', context)
 
 
+class CreateItemView(View):
 
-def create(request):
-	return None
+	form_class = CreateItemForm
+	template_name = 'items/create.html'
 
+	def get(self,request):
 
+		if request.user.is_authenticated == False :
+			return redirect('items:login')
 
+		form = self.form_class(None)
+		return render(request,self.template_name,{'form' : form})
 
+	def post(self,request):
+		
+		if request.user.is_authenticated == False :
+			print("Should not come here")
+			return redirect('items:login')
+
+		itemname = request.POST['itemname']
+		username = request.user.username
+		password  =request.POST['password']
+		user = authenticate(username=username,password=password)
+
+		if user is not None:
+			response = create_saw.cr(itemname,username)
+			print(response)
+			time.sleep(2)
+			return redirect('items:index')
 #LOGIN Stuff
 
 class UserFormView(View):
+	
 	form_class = UserForm
 	template_name = 'items/login_form.html'
+	
 	def get(self,request):
 		form = self.form_class(None)
 		return render(request,self.template_name,{'form':form})
 
 	def post(self,request):
 		form = self.form_class(request.POST)
-		print("Posting atleast")
-
 		username = request.POST['username']
 		password  =request.POST['password']
 		user = authenticate(username=username,password=password)
-		print("User authenticate")
+
 		if user is not None:
-			
 			login(request,user)
 			return redirect('items:index')
 
-		return render(request,self.template_name,{'form':form})
+		return render(request,self.template_name,{'form': form})
 			
+#Create a Logout view
+def logout_view(request):
+	if request.user.is_authenticated == False :
+		return redirect('items:login')
+
+	logout(request)
+	return redirect('items:login')
 
 
 #shift item to models
@@ -116,8 +175,6 @@ class Item(object):
 		self.p_addr = p_addr
 
 
-
-
 def _deserialize(data):
 		items = {}
 		try:
@@ -126,8 +183,8 @@ def _deserialize(data):
 				items[name] = Item(name,check,c_addr,p_addr)
 
 		except ValueError:
-			raise InternalError("Failed to deserialize items data")
-
+			pass
+			
 		return items
 
 def _deserialize_key(data):
