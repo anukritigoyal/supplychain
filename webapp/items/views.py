31a9ad8ff1f146bc1294e787b24e60c7c73cea16
GuyFawkes <isaac.prasanth@gmail.com>
2018-06-18 17:13:23 +0530

    add susan to locs

--- webapp/items/views.py
+++ webapp/items/views.py
@@ -157,7 +157,7 @@ def map(request):
 		return redirect('items:login')
 
 	#GeoLocations of users Probably change this entire charade to some other file ????
-	locations = {'admin':{'lat' : 42.34, 'longi' : -71.55}, 'Mike':{'lat':42.342, 'longi' : -71.52}}
+	locations = {'admin':{'lat' : 42.34, 'longi' : -71.55}, 'Mike':{'lat':42.342, 'longi' : -71.52}, 'Susan':{'lat':42.339 , 'longi': -71.53}}
 	response = querying.query_all_items()
 	resp = {}
 	usersdata = {}
