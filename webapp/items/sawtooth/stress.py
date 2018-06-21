4324a2ab0adc31303e207d213a922318c367007c
GuyFawkes <isaac.prasanth@gmail.com>
2018-06-21 13:02:12 +0530

    chnage name

--- webapp/items/sawtooth/stress.py
+++ webapp/items/sawtooth/stress.py
@@ -3,6 +3,6 @@ from . import send
 import time
 create.cr('exec starts','Mike')
 for i in range(1,10000):
-    create.cr(str(i),'ubuntu')
+    create.cr(str(i)+'a','ubuntu')
     time.sleep(1/1000)
 create.cr('execution is done','Mike')
