0e5ac036fde1fa14e392aec921c7e3d11739e80f
GuyFawkes <isaac.prasanth@gmail.com>
2018-06-21 13:00:01 +0530

    include sleep in stress

--- webapp/items/sawtooth/stress.py
+++ webapp/items/sawtooth/stress.py
@@ -1,7 +1,8 @@
 from . import create
 from . import send
-
+import time
 create.cr('exec starts','Mike')
 for i in range(1,10000):
     create.cr(str(i),'ubuntu')
+    time.sleep(1/1000)
 create.cr('execution is done','Mike')
