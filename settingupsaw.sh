e4536375f469d51ef46163fce2183259a3820c27
GuyFawkes <isaac.prasanth@gmail.com>
2018-06-21 18:58:17 +0530

    valid-3

--- settingupsaw.sh
+++ settingupsaw.sh
@@ -3,9 +3,9 @@
 #this is the variable command because we may have different ips
 screen -d -m -S validator sudo -u sawtooth sawtooth-validator -v \
 --bind component:tcp://127.0.0.1:4004 \
---bind network:tcp://172.31.86.24:8800 \
---endpoint tcp://34.226.140.22:8800 \
---peers tcp://54.164.118.57:8800,tcp://52.55.151.138:8800
+--bind network:tcp://172.31.81.231:8800 \
+--endpoint tcp://52.55.151.138:8800 \
+--peers tcp://54.164.118.57:8800,tcp://34.226.140.22:8800
 
 
 screen -d -m -S settings_tp sudo -u sawtooth settings-tp -v
