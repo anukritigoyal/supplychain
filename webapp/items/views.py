36bbe6e5b6152828f66ca6ce02f1e5555874be5a
GuyFawkes <isaac.prasanth@gmail.com>
2018-06-22 12:30:37 +0530

    debug empty submit

--- webapp/items/views.py
+++ webapp/items/views.py
@@ -105,7 +105,7 @@ def checked(request,itemname):
 	if request.user.is_authenticated == False :
 		return redirect('items:login')
 	
-	if request.POST['check'] == None:
+	if not 'check' in request.POST:
 		return redirect('items:detail itemname')
 	if_valid = checks.check(itemname, request.user.username,request.POST['check'],request.user.username)
 	#necessary because it takes atleast two secs for the state list to get updated
