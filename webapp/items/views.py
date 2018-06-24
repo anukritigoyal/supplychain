689e89a9f15a0ae7b3d88173f7932f874fe98a3d
GuyFawkes <isaac.prasanth@gmail.com>
2018-06-24 17:06:49 +0530

    dicts are different

--- webapp/items/views.py
+++ webapp/items/views.py
@@ -23,7 +23,7 @@ def index(request):
 	if request.user.is_authenticated == False :
 		return redirect('items:login')
 
-	if request.GET.q is None:
+	if q in request.GET:
 		resp= querying.query_user_held(request.user.username)
 		#returns from state table all the datas with c_add as username
 	else:
