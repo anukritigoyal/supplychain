7ca3c0219420448358fbe67c41c1b5263ed0af34
GuyFawkes <isaac.prasanth@gmail.com>
2018-07-06 11:25:21 +0530

    docker chanegs

--- webapp/items/views.py
+++ webapp/items/views.py
@@ -18,9 +18,10 @@ import json
 
 ###IMPORTANT SEND ALL DESERIALS TO RESPECTIVE MODULES
 ##configure randomness here
+## write a module that reads from servers.json or some other and uses it herre (sort of like config file)
 def random_server():
-	urls_list = { '1': 'http://127.0.0.1:8008' }
-	return urls_list['1']
+	urls_list = { '1': 'http://127.0.0.1:8008','2': 'http://rest-api-0:8008' }
+	return urls_list['2']
 
 def index(request):
 	
