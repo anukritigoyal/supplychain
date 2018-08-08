1e769afdb723eb22269f1a08831c01208d9ca348
Anukriti Goyal <Nikki@Anukritis-MacBook-Air.local>
2018-08-08 16:17:50 +0530

    urls

--- webapp/ptypes/urls.py
+++ webapp/ptypes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
-    path('index/', views.index, name='index')
+    url('index/', views.index, name='index'),
 ]
