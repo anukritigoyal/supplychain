9e01270b17cc3e237fd837c7de70f78742cf8d58
Anukriti Goyal <Nikki@anukritis-air.bsci.bossci.com>
2018-08-16 13:00:14 +0530

    Revert to 3e00e7f

---
+++ webapp/ptypes/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import Http404
+from django.contrib.auth import authenticate, login, logout
+from django.views import View
+import json
+
+from .forms import UserForm, User
+
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the Ptypes index.")
+
+def index(request):
+    if request.user.is_authenticated == False:
+        return redirect('items:index')
+
+    context = {'username' : request.user.username} 
+    return render(request, 'ptypes/index.html', context)
