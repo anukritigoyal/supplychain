1ea711272baa69f0f5dbc3dc3f4e765d2f07849a
Anukriti Goyal <Nikki@anukritis-air.bsci.bossci.com>
2018-08-16 15:12:06 +0530

    added forms and models

--- webapp/ptypes/forms.py
+++ webapp/ptypes/forms.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django import forms
 
+from .models import ProductType
+
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
 
@@ -8,3 +10,8 @@ class UserForm(forms.ModelForm):
         model = User
         #fields = ['username', 'password', 'dept'] 
         fields = ['username', 'password']
+
+class ProductTypeForm(forms.ModelForm):
+    class Meta:
+        model = ProductType
+        fields = ['ptype_name', 'role_name', 'check_assign']
