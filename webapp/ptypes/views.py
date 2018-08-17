432aa85c9bb6f3515797ae6a65b1f57bbe934555
Anukriti Goyal <Nikki@Anukritis-MacBook-Air.local>
2018-08-17 11:56:06 +0530

    Revert to 49ffe06

--- webapp/ptypes/views.py
+++ webapp/ptypes/views.py
@@ -6,14 +6,9 @@ from django.views import View
 import json
 
 from .forms import UserForm, User, ProductTypeForm
-from .product_type import create_product_type#, delete_product_type, create_role, delete_role, create_check, delete_check
 
 # def index(request):
 #     return HttpResponse("Hello, world. You're at the Ptypes index.")
-
-def random_server():
-    urls_list = {'1': 'http://127.0.0.1:8008', '2': 'http://rest-api-0:8008'}
-    return urls_list['2']
 
 def index(request):
     if request.user.is_authenticated == False:
@@ -23,9 +18,6 @@ def index(request):
     return render(request, 'ptypes/index.html', context)
 
 def create(request):
-    adminname = request.user.username
-    url = random_server()
-
     if request.method == "POST":
         form = ProductTypeForm(request.POST)
         if form.is_valid():
@@ -33,13 +25,6 @@ def create(request):
             ptype_name = form.cleaned_data['ptype_name']
             role_assign = form.cleaned_data['role_name']
             check_assign = form.cleaned_data['check_assign']
-
-            if role_assign is None: 
-                create_product_type.create_ptype(ptype_name, "manufacturing", adminname, url)
-            # elif role_assign is not None and check_assign is None:
-            #     create_role.create_role(ptype_name, dept, role_assign, None, adminname, url)
-            # elif role_assign is not None and check_assign is not None:
-            #     create_check.create_check(ptype_name, dept, role_assign, check_assign, adminname, url)
 
             return redirect('ptpyes/create.html')
             
