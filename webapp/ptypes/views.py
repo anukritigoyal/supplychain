603eb1e5de963a9506d758bb372c13f29012967f
Anukriti Goyal <Nikki@Anukritis-MacBook-Air.local>
2018-08-20 10:36:50 +0530

    added role and check create

--- webapp/ptypes/views.py
+++ webapp/ptypes/views.py
@@ -6,9 +6,14 @@ from django.views import View
 import json
 
 from .forms import UserForm, User, ProductTypeForm
+from .product_type import create_product_type
 
 # def index(request):
 #     return HttpResponse("Hello, world. You're at the Ptypes index.")
+
+def random_server():
+    urls_list = {'1': 'http://127.0.0.1:8008', '2': 'http://rest-api-0:8008'}
+    return urls_list['2']
 
 def index(request):
     if request.user.is_authenticated == False:
@@ -18,6 +23,9 @@ def index(request):
     return render(request, 'ptypes/index.html', context)
 
 def create(request):
+    adminname = request.user.username
+    url = random_server()
+    
     if request.method == "POST":
         form = ProductTypeForm(request.POST)
         if form.is_valid():
@@ -25,6 +33,9 @@ def create(request):
             ptype_name = form.cleaned_data['ptype_name']
             role_assign = form.cleaned_data['role_name']
             check_assign = form.cleaned_data['check_assign']
+
+            if ptype_name is not None:
+                create_product_type.create_ptype(ptype_name, 'manufacturing', adminname, url)
 
             return redirect('ptpyes/create.html')
             
