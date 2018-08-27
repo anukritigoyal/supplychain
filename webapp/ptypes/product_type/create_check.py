76aa23e236d7b728456a15c89331242c4d336097
Anukriti Goyal <Nikki@Anukritis-MacBook-Air.local>
2018-08-27 15:14:54 +0530

    updated keyfile method in create check

--- webapp/ptypes/product_type/create_check.py
+++ webapp/ptypes/product_type/create_check.py
@@ -1,11 +1,11 @@
 import os 
 from .ptype_client import PtypeClient
 
-def _get_keyfile(adminname):
-    username = adminname
-    home = os.path.expanduser("~")
-    key_dir = os.path.join(home, "sawtooth", "keys")
-    return '{}/{}.priv'.format(key_dir, username)
+def _get_keyfile(usrname):
+	username = usrname
+	home = os.path.expanduser("~")
+	key_dir = os.path.join(home, ".sawtooth", "keys")
+	return '{}/{}.priv'.format(key_dir, username)
 
 def create_check(name, dept, role, check, adminname, url):
     keyfile = _get_keyfile(adminname)
